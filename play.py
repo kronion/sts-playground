451c8f66f876dfdbe0ec6cfeb620c8c4642fde01
Zeus Kronion <kronion@users.noreply.github.com>
2022-09-10 18:44:35 -0500

    Start of a repo for using the env

---
+++ play.py
@@ -0,0 +1,27 @@
+from gym_sts.envs.base import SlayTheSpireGymEnv
+from gym_sts.spaces.observations import ObservationError
+
+from sts_playground.agents.random import RandomAgent
+
+
+env = SlayTheSpireGymEnv("../gym-sts/lib", "../gym-sts/mods", "../gym-sts/out", headless=False)
+agent = RandomAgent(env)
+SEED = 17
+agent.seed(SEED)
+
+_, info = env.reset(options={"sts_seed": "SLAY"}, return_info=True)
+print(SEED, info["sts_seed"], info["seed"])
+
+while True:
+    action = agent.predict()[0]
+    try:
+        obs, _, done, _ = env.step(action)
+    except ObservationError as e:
+        breakpoint()
+        pass
+
+    if done:
+        SEED += 1
+        agent.seed(SEED)
+        _, info = env.reset(return_info=True)
+        print(SEED, info["sts_seed"], info["seed"])
